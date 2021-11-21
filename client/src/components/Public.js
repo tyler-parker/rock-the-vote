ada8b6306d92592d9bb0c80551b7f1c042b6d341
Tyler Parker <tparker42d2@gmail.com>
2021-11-21 11:16:51 -0700

    layout is finished but my delete and edit functions are not working

--- client/src/components/Public.js
+++ client/src/components/Public.js
@@ -1,9 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import User from './User.js'
 import axios from "axios"
+import Issue from './Issue.js'
+import {
+  Box,
+  Heading,
+  Center,
+  Grid
+} from '@chakra-ui/react'
+import { UserContext } from '../context/UserProvider.js'
 
 export default function Public() {
   const userAxios = axios.create()
+  const { getAllUserIssues, allIssues } = useContext(UserContext)
+  const [users, setUsers] = useState([])
 
   userAxios.interceptors.request.use(config => {
     const token = localStorage.getItem("token")
@@ -11,20 +21,23 @@ export default function Public() {
     return config
   })
 
-  const [users, setUsers] = useState([])
 
   useEffect(() => {
-    userAxios.get("/api/users")
-    .then(res => setUsers(res.data))
-    .catch(err => console.log(err.response.data.errMsg))
+    // userAxios.get("/api/users")
+    // .then(res => setUsers(res.data))
+    // .catch(err => console.log(err.response.data.errMsg))
+
+    getAllUserIssues()
   }, [])
 
   return (
-    <div className="public">
-    <h1>Public Issues</h1>
-    <div className="publicIssues">
-      {users.map(user => <User {...user}  key={user._id}/>)}
-    </div>
-    </div>
+    <Box justify='center' align='center' m={5}>
+      <Center>
+        <Heading textDecorationLine='underline' m={5}>Public Issues</Heading>
+      </Center>
+      <Grid templateColumns='1fr'>
+          {allIssues.map(issue => <Issue {...issue}  key={issue._id}/>)}
+      </Grid>
+      </Box>
   )
 }
